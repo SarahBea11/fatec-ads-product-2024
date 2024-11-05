a2a899afab18344d526437a3d6497fd2b1736d69
SarahBea11 <sarahbealimapina@gmail.com>
2024-11-05 11:14:18 -0300

    Conexão com sql e endpoint

--- src/main/java/br/fatec/product/entities/Product.java
+++ src/main/java/br/fatec/product/entities/Product.java
@@ -12,7 +12,8 @@ public class Product implements Serializable{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
+
     private String name;
     private double price;
     private String category;
