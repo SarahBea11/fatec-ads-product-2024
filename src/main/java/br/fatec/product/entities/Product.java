79462a6cc357014a92412a5caefad6fa8494f40a
SarahBea11 <sarahbealimapina@gmail.com>
2024-11-12 10:53:01 -0300

    Criação do delete, update e save

--- src/main/java/br/fatec/product/entities/Product.java
+++ src/main/java/br/fatec/product/entities/Product.java
@@ -15,7 +15,7 @@ public class Product implements Serializable{
     private Long id;
 
     private String name;
-    private double price;
+    private Double price;
     private String category;
 
     public long getId() {
--- src/main/java/br/fatec/product/services/ProductService.java
+++ src/main/java/br/fatec/product/services/ProductService.java
@@ -26,4 +26,27 @@ public Product getProductById(long id){
     );
 }
 
+public void delete (long id){
+      if(repository.existsById(id)){
+        repository.deleteById(id);
+      }
+     else{
+      throw new EntityNotFoundException("Produto nao cadastrado");
+      }
+  }
+
+  public Product save(Product product){
+    return repository.save(product);
+  }
+
+  public void update(Product product, long id){
+    Product aux = repository.getReferenceById(id);
+
+    aux.setCategory(product.getCategory());
+    aux.setName(product.getName());
+    aux.setPrice(product.getPrice());
+
+    repository.save(aux);
+  }
+
 }
